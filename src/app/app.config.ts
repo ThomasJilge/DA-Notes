8a646a140fefda3a1a9ba39427edd7eb4be1f413
ThomasJilge <thomasjilge@gmx.de>
2024-08-20 21:10:56 +0200

    firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"danotes-97cd3","appId":"1:36999308651:web:9ae894fb2c3e1ca3ed982e","storageBucket":"danotes-97cd3.appspot.com","apiKey":"AIzaSyB_0AzwrKMIZVoTCGUUi_J4wX4nMixZShA","authDomain":"danotes-97cd3.firebaseapp.com","messagingSenderId":"36999308651"})), provideFirestore(() => getFirestore())]
 };
