30b38d069ecd6637887a88daba243f76142b1e1a
ThomasJilge <thomasjilge@gmx.de>
2024-08-21 21:41:43 +0200

    onSnapshot()

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,9 +10,36 @@ export class NoteListService {
   // trashNotes: Note[] = [];
   // normalNotes: Note[] = [];
 
+  items$;
+  items;
+  unsubList;
+  unsubSingle;
   firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+      })
+    });
+
+    this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "adf4564547shdhd"), (element) => {
+    });
+
+    this.unsubSingle();
+    this.unsubList();
+
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe( (list) => {
+      list.forEach(element => {
+        console.log(element);
+      })
+    })
+    this.items.unsubscribe();
+  }
+
 
   // const itemCollection = collection(this.firestore, 'items');
 
